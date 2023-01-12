ee68893428d75fd2e3ee6b9b30aaf87131873b44
Christiana Routon <christiana.routon@gmail.com>
2023-01-12 13:42:48 -0500

    update main photo

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <Menu class="menu"/>
       <MobileMenu class="mobile-menu"/>
-      <img class="the-brewery" alt="The brewery" src="@/assets/brewery.jpeg" />
+      <img class="the-brewery" alt="The brewery" src="@/assets/brewery_main.jpg" />
     </div>
     <Footer />
     </div>
